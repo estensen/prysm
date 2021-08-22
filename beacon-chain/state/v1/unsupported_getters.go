de5adddc0e8795c4088c70a943a5bc2fefd37f39
Håvard Anda Estensen <haavard.ae@gmail.com>
2021-08-22 22:46:01 +0200

    Errors should not be capitalized

--- beacon-chain/state/v1/unsupported_getters.go
+++ beacon-chain/state/v1/unsupported_getters.go
@@ -7,25 +7,25 @@ import (
 
 // CurrentEpochParticipation is not supported for phase 0 beacon state.
 func (b *BeaconState) CurrentEpochParticipation() ([]byte, error) {
-	return nil, errors.New("CurrentEpochParticipation is not supported for phase 0 beacon state")
+	return nil, errors.New("function CurrentEpochParticipation is not supported for phase 0 beacon state")
 }
 
 // PreviousEpochParticipation is not supported for phase 0 beacon state.
 func (b *BeaconState) PreviousEpochParticipation() ([]byte, error) {
-	return nil, errors.New("PreviousEpochParticipation is not supported for phase 0 beacon state")
+	return nil, errors.New("function PreviousEpochParticipation is not supported for phase 0 beacon state")
 }
 
 // InactivityScores is not supported for phase 0 beacon state.
 func (b *BeaconState) InactivityScores() ([]uint64, error) {
-	return nil, errors.New("InactivityScores is not supported for phase 0 beacon state")
+	return nil, errors.New("function InactivityScores is not supported for phase 0 beacon state")
 }
 
 // CurrentSyncCommittee is not supported for phase 0 beacon state.
 func (b *BeaconState) CurrentSyncCommittee() (*ethpb.SyncCommittee, error) {
-	return nil, errors.New("CurrentSyncCommittee is not supported for phase 0 beacon state")
+	return nil, errors.New("function CurrentSyncCommittee is not supported for phase 0 beacon state")
 }
 
 // NextSyncCommittee is not supported for phase 0 beacon state.
 func (b *BeaconState) NextSyncCommittee() (*ethpb.SyncCommittee, error) {
-	return nil, errors.New("NextSyncCommittee is not supported for phase 0 beacon state")
+	return nil, errors.New("function NextSyncCommittee is not supported for phase 0 beacon state")
 }
